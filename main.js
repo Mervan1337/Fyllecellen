dcdbbdfada204d17397edd679bd45a506d3f81a6
FyllecellenWebb <spons.fcellen@gmail.com>
2025-02-23 17:45:35 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("hamburgerButtons");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
