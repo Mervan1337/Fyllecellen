a10987cb1dcaeca3c2bc50455e570c5a6234d293
FyllecellenWebb <spons.fcellen@gmail.com>
2025-04-11 13:41:37 +0200

    Orginal up

---
+++ main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("hamburgerButtons");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
